9aa0df1495ed8fc0303ac3fd31a905afa09b0213
Serhio13 <imhoec@gmail.com>
2021-10-18 23:54:53 +0300

    add slider with js

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,40 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	const images = [...document.querySelectorAll('.banner__slider-img')]
+	const left = document.querySelector('#left');
+	const right = document.querySelector('#right');
+	const number = document.querySelector('.banner__pagination-number');
+
+	let el = 0
+	images.forEach((el, i) => {
+		if (i === 0) {
+			el.classList.add('active')
+		}
+	})
+	number.textContent = el
+
+	function changeSlide() {
+		images.forEach((el) => {
+			el.classList.remove('active')
+		})
+		images[el].classList.add('active')
+		number.textContent = el
+	}
+
+	left.addEventListener('click', (event) => {
+		if (!event.isTrusted) return;
+		const target = event.target;
+		el = el - 1
+		changeSlide()
+		
+	})
+
+	right.addEventListener('click', (event) => {
+		if (!event.isTrusted) return;
+		const target = event.target;
+		el = el + 1
+		changeSlide()
+	})
 
 })
